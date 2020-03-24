b4f60d8075807669d19e19593b598c315c836dba
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2020-03-24 17:42:45 -0500

    added descriptions

--- react-portfolio/src/components/AboutMe/AboutMe.js
+++ react-portfolio/src/components/AboutMe/AboutMe.js
@@ -13,7 +13,7 @@ class AboutMe extends Component {
                     <p>I’m a web developer and current student in Thinkful’s Full Stack Flex program. 
                     I have experience with HTML, CSS, JavaScript, JQuery, React, Node.js, Express, and PostgreSQL. I love continually learning and appreciate that coding 
                     keeps me on my toes. I am particularly passionate about improving the user experience and take joy in 
-                    creating beautiful, user-friendly web pages. </p>
+                    creating beautiful, user-friendly web pages. I look forward to working on a team of developers that’s going to push me to create the best products.</p>
                     <p>In my spare time, I’m likely found reading up on minimalist design, browsing the web, scuba diving, or spending 
                     time with my family. I have a degree in ecology and environmental science, fueling my love for all things nature. </p>
                     <Link to='/contact' onClick={this.props.onClick} className="lets-talk-link"><div className="lets-talk-button">Let's Talk!</div></Link>
--- react-portfolio/src/components/Projects/Projects.js
+++ react-portfolio/src/components/Projects/Projects.js
@@ -17,8 +17,7 @@ function Projects() {
                     <li><i className="fab fa-js fa-2x"></i></li>
                     <li><i className="fab fa-node fa-2x"></i></li>
                 </ul>
-                <p className="project-element">Best Brand For is a full-stack web application that allows users to make requests for products they are 
-                looking to purchase. Other users can then comment on the request to recommend their favorite brands for that product. The goal of Best Brand For is to assist users in making better buying decisions with the help of real experience. </p>
+                <p className="project-element">Best Brand For is a full-stack web application built for anybody that is on the market for a product, but is unsure which brand they should buy. I built it to make these purchase decisions easier and more informed with the help of real experience. Users can log in and make a request for products they are looking to purchase. Other users can then comment on the request to recommend their favorite brands for that product. </p>
                 <div className="project-buttons-container project-element">
                     <a className="project-button" href="https://github.com/WadeMegan/Best-Brand-For-Client" target="_blank" rel="noopener noreferrer">View Code</a>
                     <a className="project-button demo" href="https://best-brand-for.wademegan.now.sh/" target="_blank" rel="noopener noreferrer">Demo Project</a>
@@ -35,8 +34,8 @@ function Projects() {
                     <li><i className="fab fa-js fa-2x"></i></li>
                     <li><i className="fab fa-node fa-2x"></i></li>
                 </ul>
-                <p className="project-element">Drip Drop is a full-stack web application that reminds users to water their houseplants. Users can select the plants they have from a 
-                list of common houseplants. Each day, Drip Drop will check to see if any of the user's plants will need to be watered. If so, Drip Drop will send the user an SMS message as a reminder.  </p>
+                <p className="project-element">Drip Drop is a full-stack web application built for plant owners that can never remember when they need to water their plants. Users can select the plants they have from a 
+                list of common houseplants and Drip Drop will remind the user to water them. Each day, Drip Drop will check to see if any of the user's plants need to be watered. If so, Drip Drop will send the user an SMS message as a reminder. I built this web app to help prevent houseplant deaths caused by over or under watering. </p>
                 <div className="project-buttons-container project-element">
                     <a className="project-button" href="https://github.com/WadeMegan/drip-drop" target="_blank" rel="noopener noreferrer">View Code</a>
                     <a className="project-button demo" href="https://drip-drop.now.sh/" target="_blank" rel="noopener noreferrer">Demo Project</a>
@@ -52,7 +51,7 @@ function Projects() {
                     <li><i className="fab fa-css3-alt fa-2x"></i></li>
                     <li><i className="fab fa-js fa-2x"></i></li>
                 </ul>
-                <p className="project-element">Foodies on Foot allows users to search for up to 60 restaurants within walking distance from the user's given location, utilizing multiple Google Maps APIs. The app also shows the user how many grams of 
+                <p className="project-element">I built Foodies on Foot because I don't own a car, so I wanted a compiled list of restaurants close enough to walk to. Foodies on Foot is for any foodie that doesn't own a car or simply wants to minimize their environmental impact. Foodies on Foot allows users to search for up to 60 restaurants within walking distance from the user's given location, utilizing multiple Google Maps APIs. The app also shows the user how many grams of 
                 carbon dioxide emissions they are saving by walking to the restaurant instead of driving. </p>
                 <div className="project-buttons-container project-element">
                     <a className="project-button" href="https://github.com/WadeMegan/foodies-on-foot" target="_blank" rel="noopener noreferrer">View Code</a>
